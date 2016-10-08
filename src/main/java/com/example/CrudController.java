bbd179eeb88ef719d0154a2caf0360ab724d6548
toysking <tksok2009@gmail.com>
2016-10-08 09:30:56 +0900

    simple crud

--- src/main/java/com/example/CrudController.java
+++ src/main/java/com/example/CrudController.java
@@ -1,26 +1,30 @@
 package com.example;
 
-import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.MyBasedata;
 import com.example.service.MyBasedataService;
 
-//@Controller
+
+//import java.util.List;
+
+@Controller
 //@EnableAutoConfiguration
 
-@RestController
+//@RestController
 @RequestMapping("/crud")
 public class CrudController{
 
@@ -37,10 +41,28 @@ public class CrudController{
 	}
 	*/
 	
+	@Autowired
+	MyBasedataService mybasedataService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    //@ResponseStatus(HttpStatus)
+    public String showtmybasedata(Model model) {
+    	System.out.println("test ----");
+    	
+    	model.addAttribute("mybasedata", new MyBasedata());
+    
+    	return "crud";
+    }
+	
+	
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public MyBasedata insertmybasedata(@Validated @RequestBody MyBasedata mybasedata) {
-        return MyBasedataService.save(mybasedata);
+    //@RequestMapping(value = "send", method = RequestMethod.POST)
+    public String insertMybasedata(@RequestBody @ModelAttribute("mybasedata") MyBasedata mybasedata) {
+    	//System.out.println(mybasedata.getUsername());
+
+    	mybasedataService.save(mybasedata);
+    	return "crud";
     }
 }
 
--- src/main/java/com/example/entity/MyBasedata.java
+++ src/main/java/com/example/entity/MyBasedata.java
@@ -5,15 +5,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.Data;
 
-@Data
 @Entity
+//@Data
 public class MyBasedata {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
+    
     private String username;
+    
     private String password;
+    
     private String mail;
+    
     private String memo;
     
     /*
@@ -22,7 +27,47 @@ public class MyBasedata {
      * */
     
     
-    public MyBasedata(){};
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public MyBasedata(){};
     
     @Override
     public String toString() {
