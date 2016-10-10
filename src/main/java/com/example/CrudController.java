840b8023fbef2bb10f0ea1165afcd7e7c6e2c022
toysking <tksok2009@gmail.com>
2016-10-10 10:11:50 +0900

    f

--- src/main/java/com/example/CrudController.java
+++ src/main/java/com/example/CrudController.java
@@ -22,73 +22,59 @@ import com.example.service.MyBasedataService;
 
 //import java.util.List;
 
+
 @Controller
 //@EnableAutoConfiguration
-
 //@RestController
 @RequestMapping("/crud")
 public class CrudController{
-
-
-	/*
-	public String Output(HttpServletRequest request,
-            HttpStatus status,Model model){
-		System.out.println(request.getParameter("name"));
-
-		model.addAttribute("title", "titlehakoredesu");
-		model.addAttribute("values","testmodeldesuyo");
-		return "base";
-
-	}
-	*/
 	
 	@Autowired
 	MyBasedataService mybasedataService;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public String showtmybasedata(Model model) {
-    	System.out.println("test ----");
-    	
+    public String showtmybasedata(Model model) {    
     	model.addAttribute("mybasedata", new MyBasedata());
-    
     	return "crud";
     }
 	
 	
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    //@RequestMapping(value = "send", method = RequestMethod.POST)
     public String insertMybasedata(@RequestBody @ModelAttribute("mybasedata") MyBasedata mybasedata) {
-    	//System.out.println(mybasedata.getUsername());
-
     	mybasedataService.save(mybasedata);
     	return "crud";
     }
     
-
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    @ResponseStatus(HttpStatus.OK)
-    public MyBasedata updateMybasedata(@PathVariable("id") Long id, @Validated @RequestBody MyBasedata mybasedata) {
-    	mybasedata.setId(id);
-      return mybasedataService.save(mybasedata);
-    }
-      
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteMybasedata(@PathVariable("id") Long id) {
+    
+    
+    @RequestMapping(value = "/delete/{id}")
+    //@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    //@ResponseStatus(HttpStatus.OK)
+    public String deleteMybasedata(@PathVariable("id") Long id) {
     	mybasedataService.delete(id);
+    	return "redirect:/crud/all";
     }
+
+    
      
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public String getMybasedata(@PathVariable("id") Long id,Model model) {
         //return mybasedataService.find(id);
     	model.addAttribute("mybasedata", mybasedataService.find(id));
-        
     	return "crud";
     }
     
     
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public String getMybasedata(Model model) {
+        //return mybasedataService.find(id);
+    	model.addAttribute("mybasedatas", mybasedataService.findAll());
+        
+    	return "crud_view";
+    }
+        
     
     
     
